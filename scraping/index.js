83b2cfd36119890cc298749620db2290b1a14320
Brian Pelaez <pelaez_brian@hotmail.com>
2023-01-18 22:51:44 -0300

    Add a way to read json files without import

--- scraping/index.js
+++ scraping/index.js
@@ -1,22 +1,35 @@
 import * as cheerio from "cheerio";
-import {writeFile} from 'node:fs/promises';
-import path from 'node:path'
+import { writeFile, readFile } from "node:fs/promises";
+import path from "node:path";
+import fetch from "node-fetch";
+
+//import TEAMS from '../db/team.json' assert { type: "json" };
+
+const DB_PATH = path.join(process.cwd(), "./db/");
+const TEAMS = await readFile(`${DB_PATH}/teams.json`, "utf-8").then(JSON.parse);
 
 const URLS = {
   leaderboard: "https://kingsleague.pro/estadisticas/clasificacion/",
+  ligaArgentina:
+    "https://www.tycsports.com/estadisticas/liga-profesional-de-futbol/tabla-de-posiciones.html",
 };
+
+const getTeamFrom = ({ name }) => TEAMS.find((team) => team.name == name);
 
 const scrape = async (url) => {
   const res = await fetch(url);
   const html = await res.text();
+  //console.log(TEAMS)
+  //console.log(html)
   return cheerio.load(html);
 };
 
 const getLeaderBoard = async () => {
   const $ = await scrape(URLS.leaderboard);
-  const $rows = $("table tbody tr");
+  const $rows = $("table tbody tr"); // Filas donde se encuentran los datos.
 
   const LEADERBOARD_SELECTORS = {
+    // Clases de donde sacar los datos.
     team: { selector: ".fs-table-text_3", typeOf: "string" },
     wins: { selector: ".fs-table-text_4", typeOf: "number" },
     loses: { selector: ".fs-table-text_5", typeOf: "number" },
@@ -29,7 +42,7 @@ const getLeaderBoard = async () => {
   const cleanText = (text) =>
     text.replace(/\t|\n|\s:/g, "").replace(/.*:/g, "");
 
-    let leaderboard = [];
+  let leaderboard = [];
   $rows.each((index, el) => {
     const leaderBoardEntries = Object.entries(LEADERBOARD_SELECTORS).map(
       ([key, { selector, typeOf }]) => {
@@ -40,13 +53,20 @@ const getLeaderBoard = async () => {
         return [key, value];
       }
     );
-    leaderboard.push(Object.fromEntries(leaderBoardEntries));
+
+    const { team: teamName, ...leaderboardForTeam } =
+      Object.fromEntries(leaderBoardEntries);
+    const team = getTeamFrom({ name: teamName });
+      
+    leaderboard.push({ ...leaderboardForTeam, team });
   });
-  return leaderboard
+  return leaderboard;
 };
 
 const leaderboard = await getLeaderBoard();
 
-const filePath = path.join(process.cwd(), './db/leaderboard.json')
-
-await writeFile(filePath, JSON.stringify(leaderboard, null, 2), 'utf-8')
+await writeFile(
+  `${DB_PATH}/leaderboard.json`,
+  JSON.stringify(leaderboard, null, 2),
+  "utf-8"
+);
